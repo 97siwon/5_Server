e1abc4e2313796b244b5cf847782536f13b17f20
97siwon <110653581+97siwon@users.noreply.github.com>
2022-10-12 17:42:20 +0900

    .

--- JSPProject/src/main/java/edu/kh/jsp/controller/LoginServlet.java
+++ JSPProject/src/main/java/edu/kh/jsp/controller/LoginServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(ttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		// post 방식 요청 데이터 문자 인코딩 처리
 		req.setCharacterEncoding("UTF-8");
